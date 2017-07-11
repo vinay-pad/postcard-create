8a70ef9c94ab08f88b7f93436a18ac32be3fd14d
Vinay Padmanabhi <vinay@Vinays-MacBook-Pro.local>
2017-07-11 16:55:04 -0700

    feat(postcard_create): Fix lint errors

--- app/services/index.js
+++ app/services/index.js
@@ -3,10 +3,10 @@
 var Angular = require('angular');
 var Cookies = require('angular-cookies');
 
-var API     = require('./api');
-var Auth    = require('./auth');
+var API      = require('./api');
+var Auth     = require('./auth');
 var Postcard = require('./postcard');
-var Session = require('./session');
+var Session  = require('./session');
 
 module.exports = Angular.module('postcard-create.services', [
   Cookies
